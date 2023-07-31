1efd1e468e67cde44f9176517b356604c410fe14
oskal314 <artem-osipovartem3002@mail.ru>
2023-08-01 00:18:29 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './components/screens/home/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>test</>
+    <Home />
   </React.StrictMode>,
 );
